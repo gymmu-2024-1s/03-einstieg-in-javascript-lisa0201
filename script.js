6e5ab5872d88db4217040f145068c2bf5644e5a4
Lisa Waser <lisawaser05@gmail.com>
2024-10-25 12:21:37 +0000

    Aufgabe 3 erledigt

--- script.js
+++ script.js
@@ -63,3 +63,23 @@ export function aufgabe02(args) {
   return result.join("")
 }
 linkupExerciseHandler("[data-click=aufgabe02]", aufgabe02)
+
+export function aufgabe03(args) {
+  const input = args
+  const result = []
+
+  let count = 0
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    if (currentElement === "e") {
+      count = count + 1
+    } else if (currentElement === "e") {
+      count = count + 1
+    } else if (currentElement === "E") {
+      count++
+    }
+  }
+
+  return count
+}
+linkupExerciseHandler("[data-click=aufgabe03]", aufgabe03)
